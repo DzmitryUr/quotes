9741de07d3989e441bbdd1d3d35d77a2177112fc
DzmitryUr <dimaau1@gmail.com>
2018-11-06 10:49:56 +0300

    add random quote

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import quotes from './quotes.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);    
+    this.state = {
+       index: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('click');
+    this.setState({
+      index: 1
+    });
+  }
+
   render() {
     return (
       <div id="quote-box">
         <div id="text">
-          test text
+          {quotes[this.state.index].quote}
         </div>          
         <div id="author">
-          test author
+          {quotes[this.state.index].author}
         </div>
           <Button bsStyle="success" id="new-quote">
             New Quote
---
+++ src/quotes.js
@@ -0,0 +1,40 @@
+const quotes = [
+    {
+      author: 'Walt Whitman',
+      quote: 'Keep your face always toward the sunshine—and shadows will fall behind you.'
+    },
+    {
+      author: 'Amelia Barr',
+      quote: 'It is always the simple that produces the marvelous.'
+    },
+    {
+      author: 'Bertrand Russell',
+      quote: 'The world is full of magical things patiently waiting for our wits to grow sharper.'
+    },
+    {
+      author: 'Malala Yousafzai',
+      quote: 'Let us make our future now, and let us make our dreams tomorrow’s reality.'
+    },
+    {
+      author: 'Earl Nightingale',
+      quote: 'All you need is the plan, the road map, and the courage to press on to your destination.'
+    },
+    {
+      author: 'Thomas Jefferson',
+      quote: 'The glow of one warm thought is to me worth more than money.'
+    },
+    {
+       author: 'E. E. Cummings',
+       quote: 'Once we believe in ourselves, we can risk curiosity, wonder, spontaneous delight, or any experience that reveals the human spirit.'
+    },
+    {
+       author: 'John Muir',
+       quote: 'The power of imagination makes us infinite.'
+    },
+    {
+       author: 'Maya Angelou',
+       quote: 'Try to be a rainbow in someone’s cloud.'
+    },    
+  ]
+
+  export default quotes;
