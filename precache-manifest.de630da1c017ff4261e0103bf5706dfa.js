60c476ba64d380ef613f7e9a2c61a364545fce94
DzmitryUr <dimaau1@gmail.com>
2019-02-16 22:04:06 +0300

    Updates

---
+++ precache-manifest.de630da1c017ff4261e0103bf5706dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1a372811676cc4a90b0",
+    "url": "/quotes/static/js/runtime~main.f1a37281.js"
+  },
+  {
+    "revision": "44f8f3911929aeab5743",
+    "url": "/quotes/static/js/main.44f8f391.chunk.js"
+  },
+  {
+    "revision": "606e3ab9a00b2cb91794",
+    "url": "/quotes/static/js/1.606e3ab9.chunk.js"
+  },
+  {
+    "revision": "44f8f3911929aeab5743",
+    "url": "/quotes/static/css/main.fda21c3b.chunk.css"
+  },
+  {
+    "revision": "e4a90213b91ead9f919ad5aa54e155f3",
+    "url": "/quotes/index.html"
+  }
+];
